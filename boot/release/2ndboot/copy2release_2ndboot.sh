2c4f860ee6ace20d485547de3f106f69654a7776
Bongkwan Kook <kook@nexell.co.kr>
2015-10-06 17:53:13 +0900

    [Release] - Ver 0.3.6 (new naming rule)

--- boot/release/2ndboot/copy2release_2ndboot.sh
+++ boot/release/2ndboot/copy2release_2ndboot.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-SECBOOT_NAME=S5P6818_2ndboot_aarch32_DDR3_V036
+SRC_NAME=S5P6818_2ndboot_aarch32_DDR3_V036
 
 DST_NAME1=2ndboot_svt
 DST_NAME2=2ndboot_drone
-DST_NAME3=2ndboot_avn_ref
+DST_NAME3=2ndboot_asb
+DST_NAME4=2ndboot_avn_ref
 
 echo "$SECBOOT_NAME"
 
-cp ../../temporary/2ndboot/"$SECBOOT_NAME"_SDMMC.bin ./"$DST_NAME1"_sdmmc.bin
-cp ../../temporary/2ndboot/"$SECBOOT_NAME"_SDMMC.bin ./"$DST_NAME2"_sdmmc.bin
-cp ../../temporary/2ndboot/"$SECBOOT_NAME"_SDMMC.bin ./"$DST_NAME3"_sdmmc.bin
+cp ../../temporary/2ndboot/"$SRC_NAME"_ALL.bin ./"$SRC_NAME"_ALL.bin
 
-cp ../../temporary/2ndboot/"$SECBOOT_NAME"_USB.bin ./"$DST_NAME1"_usb.bin
-cp ../../temporary/2ndboot/"$SECBOOT_NAME"_USB.bin ./"$DST_NAME2"_usb.bin
-cp ../../temporary/2ndboot/"$SECBOOT_NAME"_USB.bin ./"$DST_NAME3"_usb.bin
-
-cp ../../temporary/2ndboot/"$SECBOOT_NAME"_SPI.bin ./"$DST_NAME1"_spi.bin
-cp ../../temporary/2ndboot/"$SECBOOT_NAME"_SPI.bin ./"$DST_NAME2"_spi.bin
-cp ../../temporary/2ndboot/"$SECBOOT_NAME"_SPI.bin ./"$DST_NAME3"_spi.bin
+#cp ../../temporary/2ndboot/"$SRC_NAME"_SVT_ALL.bin   ./"$DST_NAME1".bin
+cp ../../temporary/2ndboot/"$SRC_NAME"_DRONE_ALL.bin ./"$DST_NAME2".bin
+#cp ../../temporary/2ndboot/"$SRC_NAME"_ASB_ALL.bin   ./"$DST_NAME3".bin
+cp ../../temporary/2ndboot/"$SRC_NAME"_AVN_ALL.bin   ./"$DST_NAME4".bin
